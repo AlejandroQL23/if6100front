4b203fc2f8fdcd8de40e5a7496edd31c77fb3859
AlejandroQL23 <ale23200@gmail.com>
2021-10-20 19:39:00 -0600

    Propuesta de pantalla principal para el proyecto

---
+++ ProyectoIf6100/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { MainPageComponent } from './main-page/main-page.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ProyectoIf6100/src/app/material/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
+const MaterialComponens = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatMenuModule
+]
+
+@NgModule({
+  imports: [MaterialComponens],
+  exports: [MaterialComponens]
+})
+export class MaterialModule { }
